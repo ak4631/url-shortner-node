b5e91d7f223adf113c986be143f2f37f414b9c65
Akash Tiwari <efgh37716@gmail.com>
2025-09-24 22:48:22 +0530

    Mongoose Error Fix

--- connection.js
+++ connection.js
@@ -2,7 +2,9 @@ const {mongoose} = require("mongoose");
 
 async function connectMongoDb(url) {
     return mongoose.connect(url,{
-        dbName:"url_shortner"
+        dbName:"url_shortner",
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
 }
 
